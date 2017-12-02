01382072cea168bfd72e8a743f747f255d96ec9f
Andre Dargains <andre.dargains@gmail.com>
2017-12-02 22:44:30 +0000

    FIX: overlay fixes;

---
+++ source/src/components/Overlay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default (props) => (
+  <section className="overlay">
+    <span className="loading">
+      <i className="icon-airplane"></i>
+    </span>
+  </section>
+)
