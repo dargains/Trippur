5b43f9548e6d548c8c776180a29526c980f60ecd
Andre Dargains <andre.dargains@gmail.com>
2017-12-03 19:53:25 +0000

    ADD: lang.js;

---
+++ source/src/lang.js
@@ -0,0 +1,104 @@
+const lang = {
+  en: {
+    Hero: {
+      title1: "Explore.",
+      title2: "Compare. Book.",
+    }
+    SearchBar: {
+      tabs: {
+        flights: "Flights",
+        hotels: "Hotels"
+      },
+      searchButton: "Search"
+      filter: {
+        flights: {
+          from: "From",
+          whereTo: "Where to",
+          date: "Depart to Return",
+        },
+        hotels: {
+          whereTo: "Where do you want to go?",
+          date: "Checkin to checkout"
+        }
+      }
+    }
+    PersonPicker: {
+      flights: {
+        label: "passengers",
+        passengerContainer: {
+          label: "Passengers",
+          adults: "Adults",
+          children: "Children",
+          infants: "Infants",
+        }
+        classContainer: {
+          label: "Class",
+          first: "First Class",
+          business: "Business",
+          economy: "Economy"
+        }
+      },
+      hotels: {
+        label: "guests",
+        passengerContainer: {
+          label: "Guests",
+          adults: "Adults",
+          children: "Children",
+          infants: "Infants",
+        }
+      }
+    }
+  },
+  pt: {
+    Hero: {
+      title1: "Explore.",
+      title2: "Compare. Marque.",
+    }
+    SearchBar: {
+      tabs: {
+        flights: "Voos",
+        hotels: "Hotéis"
+      },
+      searchButton: "Pesquisar"
+      filter: {
+        flights: {
+          from: "De",
+          whereTo: "Para",
+          date: "Partida até retorno",
+        },
+        hotels: {
+          whereTo: "Aonde você deseja ir?",
+          date: "Partida até retorno"
+        }
+      }
+    }
+    PersonPicker: {
+      flights: {
+        label: "passageiros",
+        passengerContainer: {
+          label: "Passageiros",
+          adults: "Adultos",
+          children: "Crianças",
+          infants: "Bebés"
+        }
+        classContainer: {
+          label: "Classs",
+          first: "Primeira Classe",
+          business: "Business",
+          economy: "Economica"
+        }
+      },
+      hotels: {
+        label: "hóspedes",
+        passengerContainer: {
+          label: "Hóspedes",
+          adults: "Adultos",
+          children: "Crianças",
+          infants: "Bebés"
+        }
+      }
+    }
+  }
+}
+
+export default lang;
