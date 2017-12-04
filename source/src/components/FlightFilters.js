2d0653efb2a272917c58785850a9c3f4c7ed0fe0
Andre Dargains <andre.dargains@gmail.com>
2017-12-04 00:58:16 +0000

    UPD: flight filters active;

--- source/src/components/FlightFilters.js
+++ source/src/components/FlightFilters.js
@@ -13,33 +13,6 @@ class FlightFilters extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      price: {
-        min: 150,
-        max: 300
-      },
-      inbound: {
-        min: 800,
-        max: 1200
-      },
-      outbound: {
-        min: 500,
-        max: 700
-      },
-      stops: [
-        {
-          id: "stop0",
-          name: "stops",
-          label: "No stops"
-        }, {
-          id: "stop1",
-          name: "stops",
-          label: "1 stop"
-        }, {
-          id: "stop2",
-          name: "stops",
-          label: "+2 stops"
-        }
-      ],
       cabin: [
         {
           id: "cabin0",
@@ -64,6 +37,10 @@ class FlightFilters extends React.Component {
       price: {
         min:this.props.price_filter.min,
         max:this.props.price_filter.max
+      },
+      inbound: {
+        min:this.props.departure_day_time_filter.min,
+        max:this.props.departure_day_time_filter.max
       }
     })
   }
@@ -79,10 +56,10 @@ class FlightFilters extends React.Component {
           <InputRange
             maxValue={this.props.price_filter.max}
             minValue={this.props.price_filter.min}
-            formatLabel={value => `${value}€`}
+            formatLabel={value => `${value}${this.props.currency.symbol}`}
             value={this.state.price}
             onChange={price => this.setState({price})}
-            onChangeComplete={value => console.log(value)}
+            onChangeComplete={value => this.props.changePrice(value)}
           />
         </article>
 
@@ -107,21 +84,28 @@ class FlightFilters extends React.Component {
             onChangeComplete={value => console.log(value)}
           />
           <p className="flightLegend">Destination - <span>{this.props.arrival_airport_filters[0].code}</span></p>
-          <InputRange maxValue={1440} minValue={0} formatLabel={value => `${pad(Math.floor(value / 60))}:${pad(value % 60)}`} value={this.state.outbound} onChange={outbound => this.setState({outbound})} onChangeComplete={value => console.log(value)}/>
+          {/* <InputRange
+            maxValue={this.props.departure_day_time_filter.max}
+            minValue={this.props.departure_day_time_filter.min}
+            formatLabel={value => `${pad(Math.floor(value / 60))}:${pad(value % 60)}`}
+            value={this.state.inbound}
+            onChange={inbound => this.setState({inbound})}
+            onChangeComplete={value => console.log(value)}
+          /> */}
         </article>
 
         <hr/>
 
         <article>
           <h2 className="sidebar__legend">Cabin</h2>
-          {this.state.cabin.map(checkbox => <Checkbox key={checkbox.id} id={checkbox.id} name={checkbox.name} label={checkbox.label} handleClick={this.checkboxClick}/>)}
+          {this.state.cabin.map(checkbox => <Checkbox key={checkbox.id} id={checkbox.id} name={checkbox.name} label={checkbox.label} handleClick={this.props.changeStops}/>)}
         </article>
 
         <hr/>
 
         <article>
           <h2 className="sidebar__legend">Airlines</h2>
-          {this.props.airline_filters.map(airline => <Checkbox key={airline.code} id={airline.code} name={airline.name} label={airline.name} handleClick={this.checkboxClick}/>)}
+          {this.props.airline_filters.map(airline => <Checkbox key={airline.code} id={airline.code} name={airline.code} label={airline.name} handleClick={this.props.changeAirlines}/>)}
         </article>
 
       </div>
