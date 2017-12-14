26a477108eee5dde9a491d2afc759dfb896e8f41
Ricardo Melo <ricardo.melo@grupofullsix.pt>
2017-12-14 12:47:34 +0000

    UPD: about copy

--- source/src/lang.js
+++ source/src/lang.js
@@ -149,9 +149,11 @@ const lang = {
     About: {
       header: 'About',
       body: [
-        {type:'title',copy:'Title'},
-        {type:'text',copy:'Trippur is all about the trippurs. We love to travel. We love to explore. We love adventure. We love life. Is there really anyone who doesn’t love to travel? Probably. But the rest of us who do, have to spend smart, and choosing the best price to have the best experience is important. That is what we are all about. We are here, with only one mission, that is to help you find the best possible price for your travel and holidays, so that you can enjoy your adventure even more with a better budget to spend on what really matters instead spending it all on only airfare or hotel. With Trippur you can search thousands of the most popular travel sites at once, and choose what suits you better. Make sure you find the best deal: a hotel, flight or holiday. It’s free! Explore, compare, and book directly from the provider without extra fees. Choose what suits you better. Trippur suits any traveler.'},
-        {type:'title',copy:'SOCIAL MEDIA'},
+        //{type:'title',copy:'Title'},
+        {type:'text',copy:'Trippur is all about the trippurs. We love to travel. We love to explore. We love adventure. We love life. Is there really anyone who doesn’t love to travel? Probably. But the rest of us who do, have to spend smart, and choosing the best price to have the best experience is important. That is what we are all about.'},
+        {type:'text',copy:'We are here, with only one mission, that is to help you find the best possible price for your travel and holidays, so that you can enjoy your adventure even more with a better budget to spend on what really matters instead spending it all on only airfare or hotel.'},
+        {type:'text',copy:'With Trippur you can search thousands of the most popular travel sites at once, and choose what suits you better. Make sure you find the best deal. It’s free! Search, compare, and book directly from the provider without extra fees. Choose what suits you better. Trippur suits any traveler.'},
+        {type:'title',copy:'Get in touch'},
         {type:'text',copy:'Sometimes forms or email aren’t what you need, so check us out on Facebook, Twitter, Instagram and Google+ for some further travel inspiration. You can also leave comments and questions too, we’re always listening.'},
         {type:'list', copy:['Facebook','Twitter','Instagram']}
       ]
